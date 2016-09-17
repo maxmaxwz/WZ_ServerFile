0463bd56eaef4c59a62c6cc25355ab25229583e1
WZ_ServerFile <1078777524@qq.com>
2016-09-17 20:55:23 +0800

    创建文件

---
+++ 异步加载网络图片/异步加载网络图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  异步加载网络图片
+//
+//  Created by 汪舟 on 16/9/17.
+//  Copyright © 2016年 wangzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
